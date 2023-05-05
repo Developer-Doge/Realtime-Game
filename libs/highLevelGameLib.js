da0be99a37ee4698d969c275d65b55909081bf7b
WiiDude <79222355+Developer-Doge@users.noreply.github.com>
2023-05-05 23:11:32 +0000

    Added score system, and misc fixes
    Fixed:
    react/no-unescaped-entities
    fixed Syntax errors

--- libs/highLevelGameLib.js
+++ libs/highLevelGameLib.js
@@ -8,8 +8,9 @@ import { database } from "./realtime";
 
 export const joinGame = (gameId, playerName) => {
   const dbRef = ref(database, `games/${gameId}/players`);
+  const scoresRef = ref(database, `games/${gameId}/scores`);
   
-  // Get current players
+  // Get current players and scores
   get(dbRef).then((snapshot) => {
     if (snapshot.exists()) {
       // Add player to current players
@@ -17,18 +18,55 @@ export const joinGame = (gameId, playerName) => {
       players.push(playerName);
       set(dbRef, players);
 
+      // Add score for the new player
+      get(scoresRef).then((scoresSnapshot) => {
+        if (scoresSnapshot.exists()) {
+          const scores = scoresSnapshot.val();
+          scores[playerName] = 0; // Initialize score to 0
+          set(scoresRef, scores);
+        } else {
+          // No scores exist, create a new object with the new player's score
+          set(scoresRef, { [playerName]: 0 });
+        }
+      }).catch((error) => {
+        console.error(error);
+      });
+
       // Update isInGame recoil state
 
     } else {
       // Create new player
       set(dbRef, [playerName]);
-      
+
+      // Create new scores object with the new player's score
+      set(scoresRef, { [playerName]: 0 });
+
       // Update the isInGame recoil state
     }
   }).catch((error) => {
     console.error(error);
   });
 }
+
+export const setScore = (gameId, playerName, score) => {
+  const scoresRef = ref(database, `games/${gameId}/scores/${playerName}`);
+  set(scoresRef, score).catch((error) => {
+    console.error(error);
+  });
+}
+
+export const addScore = (gameId, playerName, score) => {
+  const scoresRef = ref(database, `games/${gameId}/scores/${playerName}`);
+  get(scoresRef).then((snapshot) => {
+    if (snapshot.exists()) {
+      const currentScore = snapshot.val();
+      setScore(gameId, playerName, currentScore + score); // Update score using setScore function
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+}
+
 
 export const updateState = (gameId, state) => {
   const dbRef = ref(database, `games/${gameId}/state`);
@@ -50,3 +88,21 @@ export const readGameState = (gameId) => {
 
   return data;
 }
+
+// Read scores
+export const readScores = (gameId, playerName) => {
+  const dbRef = ref(database, `games/${gameId}/scores/${playerName}/`);
+  const data = get(dbRef)
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        return snapshot.val();
+      } else {
+        return {};
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  return data;
+};
